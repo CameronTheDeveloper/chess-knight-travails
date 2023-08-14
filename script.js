148c6e21e511e0bc85df0e170071b14c9cd3ac96
CameronTheDeveloper <Cameronposey@outlook.com>
2023-08-13 18:06:12 -0700

    Add chessboard positions
    This commit introduces functions for establishing connections between adjacent moves, although their logic is currently being assembled

--- script.js
+++ script.js
@@ -1,42 +1,61 @@
 const board = () => {
     return {
-        moves: new Map(),   // Holds adjacent moves
-        addMove(move) {
+        moves: new Map(),
+        addPos(move) {
             this.moves.set(move, []);
         },
-        addEdge(move1, move2) {
+        addMove(move1, move2) {
+            console.log('Move1 Move2: ', move1, move2);
+            console.log(this.moves.get(move1));
+            console.log(this.moves.get(move2));
+
             this.moves.get(move1).push(move2);
-            this.moves.get(move2).push(move1);  //Undirected
+            // this.moves.get(move2).push(move1);  //Undirected
         },
         showMoves() {
-            const vertexKeys = this.moves.keys();     //Get vertices
-
-            for (let vKey of vertexKeys) {     // Iterate vertices
-                const adjMoves = this.moves.get(vKey);
-                console.log(adjMoves);
+            console.log(this.moves);
+            const vertexKey = this.moves.keys();
+            for (let move of vertexKey) {
+                const adjMoves = this.moves.get(move);
                 for (let adjMove of adjMoves) {
+                    // console.log(move, '=>', adjMove);
                     // console.log(adjMove);
                 }
             }
         },
-        getMove(xPos, yPos) {
-            let vertexKeys = this.moves.keys();
-            for (let move of vertexKeys) {
-                if (move[0] === xPos && move[1] === yPos) {
-                    // console.log('MOVE', move);
-                    return move;
+        findShortestPath(startingPos, destination) {
+            //if visited == false{}
+            let queue = [];
+            let visited = {};
+            let pathAr = [];
+
+            let readQueue = (pos) => {
+                queue.push(pos);
+                while (queue.length > 0) {
+                    let currentNode = queue.shift();
+                    if (!currentNode.visited) {
+                        visited[currentNode] = true;
+                        pathAr.push(currentNode);
+                        if (currentNode[0] === destination[0] &&
+                            currentNode[1] === destination[1]) {
+                            return pathAr;
+                        }
+                        // readQueue(currentNode);
+                    }
+
                 }
-            }
+            };
+
+            readQueue(move);
+            //Need to stop visiting the same adjList
+            // let move = this.getMove(startingPos[0], startingPos[1]);
+
+            let adjMoves = this.moves.get(move);
+            for (let adjMove of adjMoves) {
+                readQueue(adjMove);
+            }   //Check if readQueue needs recursion even though this loop itterates
+            return pathAr;//
         },
-        findShortestPath(startingPos, destination, movesAr = []) {
-            movesAr.push(startingPos);
-            if (startingPos === destination) {
-                return movesAr;
-            }
-            for (let adjPos of startingPos) {
-                this.findShortestPath(adjPos, destination, movesAr);
-            }
-        }
     };
 };
 
@@ -54,14 +73,15 @@ const getMovesArray = () => {
 const connectMoves = (chessBoard, moveAr) => {
     let moveX = [2, 1, -1, -2, -2, -1, 1, 2];
     let moveY = [1, 2, 2, 1, -1, -2, -2, -1];
-
-    for (let move of moveAr) {
+    // let visited = {};
+    for (let move in moveAr) {
         for (let i = 0; i < moveX.length; i++) {
             let xPos = move[0] + moveX[i];
             let yPos = move[1] + moveY[i];
             if (xPos >= 1 && yPos >= 1 && xPos <= 8 && yPos <= 8) {
-                let adjMove = chessBoard.getMove(xPos, yPos);
-                chessBoard.addEdge(move, adjMove);
+                let adjMove = [xPos, yPos];
+                chessBoard.addMove(move, adjMove);
+                // console.log(xPos, yPos);
             }
         }
     }
@@ -73,11 +93,19 @@ const driver = () => {
 
     for (let i = 0; i < moveAr.length; i++) {
         let move = moveAr[i];
-        chessBoard.addMove(move);
+        chessBoard.addPos(move);
     }
-
-    connectMoves(chessBoard, moveAr);
+    // connectMoves(chessBoard, moveAr);
     chessBoard.showMoves();
+
+
+    // connectMoves(chessBoard, moveAr);   //Stop using moveAr
+    console.log();
+    const start = [1, 1];
+    const dest = [5, 5];
+    // console.log(start, dest);
+    // chessBoard.showMoves();
+    // console.log('Path: ', chessBoard.findShortestPath(start, dest));
 };
 
 driver();
